7ed09b9fd07d05eaf75c7667325b9af18ec5b8ec
lepis <leooblivion24@gmail.com>
2020-08-03 14:00:24 +0700

    Updates

---
+++ precache-manifest.3d14b72379ee50a2be63f5b6f3f1b85b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97b16955394e410bbaf500eb71d851d9",
+    "url": "/clone-uber/index.html"
+  },
+  {
+    "revision": "cb9f2d1923bb205d47f8",
+    "url": "/clone-uber/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a0e4ba8aeeaa401a002d",
+    "url": "/clone-uber/static/css/main.2060adbb.chunk.css"
+  },
+  {
+    "revision": "cb9f2d1923bb205d47f8",
+    "url": "/clone-uber/static/js/2.aaf1ea48.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/clone-uber/static/js/2.aaf1ea48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0e4ba8aeeaa401a002d",
+    "url": "/clone-uber/static/js/main.1610a859.chunk.js"
+  },
+  {
+    "revision": "517ad33c683a09150352",
+    "url": "/clone-uber/static/js/runtime-main.ed1841c6.js"
+  }
+]);
